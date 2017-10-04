63b664026b9ef1a3ff36fd4652eaa4b64be393a6
George Petrov <george@supplesoftware.com>
2017-10-04 14:17:54 +0100

    Fixing the effects case classes

--- src/main/scala/effects.scala
+++ src/main/scala/effects.scala
@@ -15,25 +15,32 @@ object effects {
   case class PutStrLn(line: String) extends Console[Unit]
   case class GetStrLn() extends Console[String]
 
-  case class Sequence[A0](first: Console[A0], f: A0 => Console[A]) extends Console[A] // bind
+  case class Sequence[A0, A](first: Console[A0], f: A => Console[A]) extends Console[A0] // bind
   case class Pure[A](value: A) extends Console[A] // point
   case class Map[A0, A](program: Console[A0], f: A0 => A) extends Console[A] //map
   
-
+/*. No flatMap on these yet
   val console: Console[Unit] = 
     for {
       _ <- PutStrLn("What is your name")
       n <- GetStrLn()
       _ <- PutStrLn("Hello, " + n)
     } yield ()
-    
+*/
 }
 
 
 object exercise1 {
-  final case class IO[A](/* ??? */)
 
-  implicit val IOMonad: Monad[IO] = ???
+  // final case class IO[A](unsafePerformIO: () => A)
+
+  // def putStrLn(line: String): IO[Unit] = 
+  //   IO(unsafePerformIO = () => println(line))
+  
+  // def getStrLb(): IO[String] = 
+  //   IO(unsafePerformIO = () => )
+
+  //implicit val IOMonad: Monad[IO] = ???
 }
 
 object exercise2 {

b31634c1324b0cbdc2dd928b0a36c76b222146cb
George Petrov <george@supplesoftware.com>
2017-10-04 14:55:20 +0100

    Adding an IO monad and effect composition

--- src/main/scala/effects.scala
+++ src/main/scala/effects.scala
@@ -32,16 +32,77 @@ object effects {
 
 object exercise1 {
 
-  // final case class IO[A](unsafePerformIO: () => A)
+  final case class IO[A](unsafePerformIO: () => A) { self => 
+    final def map[B](f: A => B): IO[B] = 
+      IO(  () => f(self.unsafePerformIO())  )
+      
+    final def flatMap[B](f: A => IO[B]): IO[B] = 
+      IO(() => f(self.unsafePerformIO()).unsafePerformIO())
 
-  // def putStrLn(line: String): IO[Unit] = 
-  //   IO(unsafePerformIO = () => println(line))
+    final def zip[B](that: IO[B]): IO[(A, B)] = 
+      self.flatMap(a => that.map(b => (a, b)))
+  }
+
+  object IO {
+    def point[A](a: => A): IO[A] = IO(() => a)
+  }
+
+  def putStrLn(line: String): IO[Unit] = 
+    IO(unsafePerformIO = () => println(line))
   
-  // def getStrLb(): IO[String] = 
-  //   IO(unsafePerformIO = () => )
+  def getStrLn(): IO[String] = 
+    IO(unsafePerformIO = () => readLine())
+
+
+
+  val program: IO[String] = 
+    for {
+      _ <- putStrLn("What is your name?")
+      n <- getStrLn()
+      _ <- putStrLn("Hello, " + n)
+    } yield n
+
 
   //implicit val IOMonad: Monad[IO] = ???
 }
+
+
+object fine { 
+
+  import exercise1._
+
+  trait ConsoleIO[F[_]] {
+    def putStrLn(line: String): F[Unit]
+    def getStrLn(): F[String]
+  }
+
+  implicit val IOConsoleIO = new ConsoleIO[IO] {
+    def putStrLn(line: String): IO[Unit] = 
+      IO(unsafePerformIO = () => println(line))
+    
+    def getStrLn(): IO[String] = 
+      IO(unsafePerformIO = () => readLine())
+  }
+
+  trait Logging[F[_]] {
+    def log(line: => String, level: LogLevel)
+  }
+
+  sealed trait LogLevel
+  case object INFO extends LogLevel
+  case object DEBUG extends LogLevel
+
+
+  trait HttpClient[F[_]] {
+    def request(method: String, url: String): Response
+  }
+
+  sealed trait Response
+
+  def subprogram[F[_]: ConsoleIO, Logging]: F[String] = ???
+
+}
+
 
 object exercise2 {
   import exercise1._
